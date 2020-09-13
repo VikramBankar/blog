547004a71918c79d57e58c15d412c0deb754e5db
Vikram Bankar <vkmbankar@gmail.com>
2020-09-13 11:09:44 +0530

    Add project

---
+++ build/precache-manifest.bc047293daef487d17ce5645df859233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f369f407871a2ebde7c65d4edaa18e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f7fe22adaf3fd64960",
+    "url": "/static/css/main.91c05e56.chunk.css"
+  },
+  {
+    "revision": "e4e76e87be435083dfc5",
+    "url": "/static/js/2.37d91de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37d91de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f7fe22adaf3fd64960",
+    "url": "/static/js/main.07a2e244.chunk.js"
+  },
+  {
+    "revision": "4b4be035672f5f8bc943",
+    "url": "/static/js/runtime-main.6f26bf25.js"
+  }
+]);
